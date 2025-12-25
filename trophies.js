624e6bde6f946b4af8609f53dbd2e256fad0d7f6
muhammed-husein <muhammedhsen17@gmail.com>
2025-12-24 19:05:17 -0800

    Add files via upload

---
+++ trophies.js
@@ -0,0 +1,9 @@
+window.TROPHY_DATA = [
+  { id: 'uefa-champions-league', name: "UEFA Champions League", count: 15, years: [1956,1957,1958,1959,1960,1966,1998,2000,2002,2014,2016,2017,2018,2022,2024], image: "players/images/UCL.jpg" },
+  { id: 'la-liga', name: "La Liga", count: 36, years: [1932,1933,1954,1955,1957,1958,1961,1962,1963,1964,1965,1967,1968,1969,1972,1975,1976,1978,1979,1980,1986,1987,1988,1989,1990,1995,1997,2001,2003,2007,2008,2012,2017,2020,2022,2024], image: "players/images/laliga.jpg" },
+  { id: 'copa-del-rey', name: "Copa del Rey", count: 20, years: [1905,1906,1907,1908,1917,1934,1936,1946,1947,1962,1970,1974,1975,1980,1982,1989,1993,2011,2014,2023], image: "players/images/COPADELREY.jpg" },
+  { id: 'fifa-club-world-cup', name: "FIFA Club World Cup", count: 5, years: [2014,2016,2017,2018,2022], image: "players/images/clubworldcup.jpg" },
+  { id: 'uefa-super-cup', name: "UEFA Super Cup", count: 6, years: [2002,2014,2016,2017,2022,2024], image: "players/images/super.jpg" },
+  { id: 'intercontinental-cup', name: "Intercontinental Cup", count: 3, years: [1960,1998,2002], image: "players/images/intercontinental-cup.jpg" },
+  { id: 'FIFA intercontinental-cup', name: "FIFA Intercontinental Cup", count: 1, years: [2024], image: "players/images/FIFA-intercontinental-cup.jpg" }
+];
